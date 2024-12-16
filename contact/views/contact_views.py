77bec8263da633f661c27bf6f23b8358a6051785
Iago <iago.0206@hotmail.com>
2024-12-16 14:59:42 -0300

    'pagination'

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,44 +1,29 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from contact.models import Contact
+from django.core.paginator import Paginator
 from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 
-def index (request):
+from contact.models import Contact
+
+
+def index(request):
     contacts = Contact.objects \
-    .filter(show=True)\
-    .order_by('-id')[10:20]
-    
+        .filter(show=True)\
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts' : contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
     return render(
-
-        request,  
-        'contact/index.html',
-         context
-    )
-
-
-def contact(request, contact_id):
-    
-    single_contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
-    )
-
-    contact_name = f'{single_contact.first_name} {single_contact.last_name}'
-
-    context = {
-        'contact': single_contact,
-        'site_title': contact_name,
-    }
-
-    return render(
         request,
-        'contact/contact.html',
+        'contact/index.html',
         context
     )
-
 
 
 def search(request):
@@ -57,13 +42,37 @@ def search(request):
         )\
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
-        'site_title': 'Search - '
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
+    context = {
+        'contact': single_contact,
+        'site_title': site_title
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
