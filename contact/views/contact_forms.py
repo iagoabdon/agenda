ddb1a06c7cde97be60fdfaf68da0b7f2731b0fbd
Iago <iago.0206@hotmail.com>
2024-12-17 16:09:34 -0300

    'outrasConfForm'

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,19 +1,40 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
+from django import forms
 
 from contact.models import Contact
 
+#### copiando meu formulario contact 
+class ContactForm(forms.ModelForm):
+      class Meta:
+             model = Contact
+             fields = (
+                    'first_name', 'last_name', 'phone', 
+                    )
+             
+
+
 
 def create(request):
-       
+       if request.method == 'POST':
+            context = {
+              'form': ContactForm(request.POST)
+            }
+            
+            return render(
+             request,
+             'contact/create.html',
+             context )
+     
+
+       ## quando form e atualizado usando o metodo get
        context = {
-                
+       'form' : ContactForm()
        }
-    
+
        return render(
-        request,
-        'contact/create.html',
-        context
-         
-    )
+             request,
+             'contact/create.html',
+             context
+       )
