070f31f235ae6b04e7d9dd8ac4a34a334d781842
IagoAbdon <iago.0206@hotmail.com>
2024-12-23 17:08:01 -0300

    'addDel

--- contact/urls.py
+++ contact/urls.py
@@ -6,10 +6,13 @@ app_name = 'contact'
 
 urlpatterns = [
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),  # type:ignore
+   
     path('search/', views.search, name='search'), # type:ignore 
     path('', views.index, name='index'),  # type:ignore
 
    #### (Create)CRUD ####
 
      path('contact/create/', views.create, name='create'),  # type:ignore
-]
+     path('contact/<int:contact_id>/update/', views.update, name='update'),
+     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+]  
