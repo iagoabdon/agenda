b38959a3fa16f71357500f71ff8657df5451a3aa
Iago <iago.0206@hotmail.com>
2025-01-03 10:51:02 -0300

    'addcamposdoregister'

--- contact/forms.py
+++ contact/forms.py
@@ -4,6 +4,7 @@ from . import models
 
 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class ContactForm(forms.ModelForm):
@@ -73,3 +74,30 @@ class ContactForm(forms.ModelForm):
     
 class RegisterForm(UserCreationForm):
     ...
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este e-mail', code='invalid')
+            )
+
+        return email
