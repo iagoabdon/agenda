99b5e04bbf97e853419cdedaa6f3d01af9cc22c1
Iago <iago.0206@hotmail.com>
2025-01-03 11:39:33 -0300

    'mensagemdecadastro'

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.contrib import  messages
+from django.shortcuts import redirect, render
 
 from contact.forms import RegisterForm
 
+
 def register(request):
     form = RegisterForm()
+
     if request.method == 'POST':
         form = RegisterForm(request.POST)
+
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuário registrado')
+            return redirect('contact:index')
+
     return render(
         request,
         'contact/register.html',
@@ -15,3 +22,4 @@ def register(request):
             'form': form
         }
     )
+    
